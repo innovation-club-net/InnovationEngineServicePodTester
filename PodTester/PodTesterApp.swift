4d76ad84212671f09fbbaa5fe7ababc5223bab09
beezital <63583702+beezital@users.noreply.github.com>
2022-12-23 22:41:21 +0100

    Initial Commit

---
+++ PodTester/PodTesterApp.swift
@@ -0,0 +1,17 @@
+//
+//  PodTesterApp.swift
+//  PodTester
+//
+//  Created by Fred on 23.12.22.
+//
+
+import SwiftUI
+
+@main
+struct PodTesterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
