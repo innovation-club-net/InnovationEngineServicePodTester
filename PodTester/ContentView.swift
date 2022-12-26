4f8f2a91757401159cf993c858569119dd8b74be
beezital <63583702+beezital@users.noreply.github.com>
2022-12-26 01:04:58 +0100

    auto scroll to experiment result + better error message

--- PodTester/ContentView.swift
+++ PodTester/ContentView.swift
@@ -10,77 +10,131 @@ import SwiftUI
 struct ContentView: View {
     
     @StateObject var viewModel = InnovationEngineViewModel()
-    @State var environment: String = "dev"
-    @State var timeout: String = "500"
+    @State var environment: String = ""
+    @State var timeout: String = ""
     @State var clientId: String = String(Int(Date().timeIntervalSince1970 * 1000))
     @State var resultDescription: String = ""
     @State var experimentView: ExperimentView?
 
+    @Namespace var bottomID
     
     var body: some View {
         ZStack {
-            VStack(alignment: .leading) {
-                Text(viewModel.loaderServer ?? "")
-                TextField("Environment", text: $environment)
-                TextField("Timeout", text: $timeout)
-                TextField("ScreenId", text: $viewModel.screenId)
-                Text(clientId)
-                Button("Restart") {
-                    restart()
+            ScrollViewReader { scrollViewProxy in
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("configLoaderServer")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text(viewModel.loaderServer ?? "")
+                                .padding(5)
+                        }
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("configTimeout")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text(timeout)
+                                .padding(5)
+                        }
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("configEnvironment")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text(environment)
+                                .padding(5)
+                        }
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Screen ID passed to getExperiments()")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            TextField("ScreenId", text: $viewModel.screenId)
+                                .padding(10)
+                                .background(.background)
+                                .clipShape(RoundedRectangle(cornerRadius: 10))
+                        }
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("configClientId (*)")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text(clientId)
+                                .padding(5)
+                            Text("(*) newly generated on each request for testing purposes")
+                                .font(.subheadline)
+                                .italic()
+                                .foregroundColor(.secondary)
+                        }
+                        Button("GetExperiment") {
+                            getExperiment(scrollViewProxy: scrollViewProxy)
+                        }
+                        .padding(10)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .background(.background)
+                        .clipShape(RoundedRectangle(cornerRadius: 10))
+                        
+                        VStack(alignment: .leading, spacing: 5) {
+                            Text("Result")
+                                .font(.subheadline)
+                                .foregroundColor(.secondary)
+                            Text(resultDescription)
+                                .padding(5)
+                                .frame(maxWidth: .infinity, alignment: .center)
+                        }
+                        .id(bottomID)
+                        Spacer()
+                    }
+                    .padding()
                 }
-                Text(resultDescription)
+                .task {
+                    timeout = String(describing: viewModel.innovationEngine.configTimeout)
+                    environment = viewModel.innovationEngine.configEnvironment ?? ""
+
+                    // varies with each screen/view of the app
+                    viewModel.screenId = "dashboard"
+                    
+                    // perform a first initial request
+                    getExperiment(scrollViewProxy: scrollViewProxy)
+                }
             }
+            
+            // Cover the config screen with the experiment's WebView
             if let experimentView = experimentView {
                 experimentView
             }
         }
-        .padding()
-        .onChange(of: environment) { newEnvironment in
-            viewModel.configureEnvironment(newEnvironment)
-        }
-        .onChange(of: timeout) { newTimout in
-            viewModel.configureTimeout(newTimout)
-        }
-        .onAppear() {
-            // typically set once at the start of the app
-            viewModel.configureEnvironment(environment)
-            viewModel.configureTimeout(timeout)
-            
-            // varies with each screen/view of the app
-            viewModel.screenId = "dashboard"
-            
-            // perform a first initial request
-            restart()
-        }
+        .background(Color(UIColor.secondarySystemBackground))
+
     }
     
-    func restart() {
+    func getExperiment(scrollViewProxy: ScrollViewProxy) {
+        resultDescription = ""
         clientId = String(Int(Date().timeIntervalSince1970 * 1000))
         viewModel.configureClientId(clientId)
         viewModel.getExperiments() { getExperimentsResult in
             switch getExperimentsResult {
             case .failure(let error):
                 // handle the error
-                print("\(error)")
-                resultDescription = "\(error.localizedDescription)"
+                resultDescription = InnovationEngineViewModel.getMessageForError(error)
+                print(resultDescription)
+                scrollViewProxy.scrollTo(bottomID)
 
             case .success(let experiments):
                 // This example only considers the first entry of the array of Experiments
                 guard let experiment = experiments[0] else {
                     print("No experiment returned")
-//                    DispatchQueue.main.async {
-//                        self.viewModel.setError("No experiment returned")
-//                    }
+                    resultDescription = "No experiment returned"
+                    scrollViewProxy.scrollTo(bottomID)
                     return
                 }
-                // start the experiment
                 print("experiments[0] \(experiment)")
-//                self.startExperiment(experiment)
+
+                // start the experiment in a view
                 experimentView = ExperimentView(experiment: experiment) { startExperimentResult in
                     switch startExperimentResult {
                     case .failure(let error):
                         // handle the error
                         resultDescription = "\(error.localizedDescription)"
+                        scrollViewProxy.scrollTo(bottomID)
 
                     case .success(let event):
                         // handle the event
@@ -88,6 +142,7 @@ struct ContentView: View {
                         print(event.treatmentUuid!)
                         print(event.interaction!)
                         resultDescription = "\(event.interaction ?? "?")"
+                        scrollViewProxy.scrollTo(bottomID)
                     }
 
                     experimentView = nil
@@ -96,6 +151,7 @@ struct ContentView: View {
 
         }
     }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -103,3 +159,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+
--- PodTester/InnovationEngineViewModel.swift
+++ PodTester/InnovationEngineViewModel.swift
@@ -12,7 +12,7 @@ class InnovationEngineViewModel: NSObject, ObservableObject {
     
     @Published var screenId: String = ""
         
-    private var innovationEngine: InnovationEngine
+    var innovationEngine: InnovationEngine
     
     
     var loaderServer: String? {
@@ -24,6 +24,12 @@ class InnovationEngineViewModel: NSObject, ObservableObject {
         
         let loaderServer = Bundle.main.object(forInfoDictionaryKey: "NVTNCLB_LOADER_SERVER") as? String ?? ""
         innovationEngine.configLoaderServer = !loaderServer.isEmpty ? loaderServer : "https://your-instance.innovation-club.net"
+        
+        let environment = Bundle.main.object(forInfoDictionaryKey: "NVTNCLB_ENVIRONMENT") as? String ?? ""
+        innovationEngine.configEnvironment = !environment.isEmpty ? environment : "dev"
+        
+        let timeout = Bundle.main.object(forInfoDictionaryKey: "NVTNCLB_TIMEOUT") as? String ?? ""
+        innovationEngine.configTimeout = !timeout.isEmpty ? Int(timeout) ?? 500 : 500
     }
     
     
@@ -65,4 +71,21 @@ class InnovationEngineViewModel: NSObject, ObservableObject {
              */
         }
     }
+    
+    
+    ///
+    ///
+    static func getMessageForError(_ error: Error) -> String {
+        switch error as? RequestError {
+        case .error(let error):
+            return error.localizedDescription
+        case .parsing:
+            return "InnovationEngine parsing failed"
+        case .webPageEmpty:
+            return "InnovationEngine web page is empty"
+        case .none:
+            fatalError("InnovationEngine request failed \n error in wrong format")
+        }
+    }
+
 }
